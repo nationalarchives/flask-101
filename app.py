cae805a21777ee2b66aeacaa2750813df4ba4111
Gwyn Jones <m@gwnjns.com>
2019-11-26 08:15:41 +0000

    Add dynamic route

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Life is short, buy the guitar'
+
+@app.route('/<param>')
+def thing(param):
+    return 'Life is short, buy the {}'.format(param)
